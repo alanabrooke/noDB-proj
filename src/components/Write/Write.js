f0275b1f8cb634c1b1d186b340baff8b40d2decd
dovahlana <atennison25@gmail.com>
2020-01-30 20:15:28 -0600

    after form

--- src/components/Write/Write.js
+++ src/components/Write/Write.js
@@ -6,17 +6,17 @@ class Write extends Component {
         this.state = {
             val: ''
         }
-
-
-
-
     }
+
+
   render() {
     return (
       <div>
         <h1>Confession time!</h1>
+        <form>
         <input placeholder='Write confession here'/>
         <button id = 'submit'>Submit</button>
+        </form>
       </div>
     );
   }
